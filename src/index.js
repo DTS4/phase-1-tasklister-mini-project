a88707e71b92e8aa3060b145ee9225f63ac3bc20
Keith <keith.githinji@student.moringaschool.com>
2024-10-06 14:39:07 +0300

    Completed the lab

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
   let form = document.querySelector('#create-task-form');
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let todo = document.getElementById(`new-task-description`).value;
-    buildToDo(todo);
-    form.reset()
+
+    let todo = document.getElementById('new-task-description').value;
+    let priority = document.getElementById('priority').value;
+    buildToDo(todo, priority);
+
+    form.reset();
   });
 });
 
-
-function buildToDo(todo) {
+function buildToDo(todo, priority) {
   let li = document.createElement('li');
-  let btn = document.createElement('button')
+
+  li.textContent = todo;
+  if (priority === 'high') {
+    li.style.color = 'red';
+  } else if (priority === 'medium') {
+    li.style.color = 'yellow';
+  } else {
+    li.style.color = 'green';
+  }
+
+  let btn = document.createElement('button');
   btn.textContent = 'x';
   btn.addEventListener('click', handleDelete);
-  li.textContent = todo;
+
   li.appendChild(btn);
   document.querySelector('#tasks').appendChild(li);
 }
